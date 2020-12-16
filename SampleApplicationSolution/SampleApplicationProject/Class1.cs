e87242c16e9a3509094f7059f35aa295da245c02
Rohit HR <rohit.hr@lntinfotech.com>
2020-12-16 10:44:24 +0530

    Added class1

---
+++ SampleApplicationSolution/SampleApplicationProject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplicationProject
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class 1 created in local repo");
+        }
+    }
+}
