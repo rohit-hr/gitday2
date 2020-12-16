3e6469ac082e7b083c33d014a4a4d10304e02889
rohit-hr <76032617+rohit-hr@users.noreply.github.com>
2020-12-16 10:42:40 +0530

    Create Class2.cs
    Class 2 created

---
+++ SampleApplicationSolution/SampleApplicationProject/Class2.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplicationProject
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class 2 created in remote repo");
+        }
+    }
+}
